36032fe4df7e61811bb5130125cb5e496dbbd29d
daryl-bishop <daryl.bishop@hotmail.com>
2021-06-30 11:59:24 +0100

    updated calculator

--- operator_task/calculator.java
+++ operator_task/calculator.java
@@ -6,4 +6,21 @@ public class calculator {
 		int sum = a + b;
 		return sum;
 	}
+
+	public static int multiply(int c, int d) {
+		int multiply = c * d;
+		return multiply;
+	}
+
+	public static int divide(int e, int f) {
+		int divide = e / f;
+		return divide;
+	}
+
+	public static int subtract(int g, int h) {
+		int subtract = g - h;
+		return subtract;
+
+	}
+
 }
--- operator_task/operator_task.java
+++ operator_task/operator_task.java
@@ -6,6 +6,15 @@ public class operator_task {
 		System.out.println(calculator.add(1, 2));
 		System.out.println(calculator.add(11, 2));
 		System.out.println(calculator.add(18, 2));
+		System.out.println(calculator.multiply(2, 4));
+		System.out.println(calculator.multiply(5, 6));
+		System.out.println(calculator.multiply(10, 8));
+		System.out.println(calculator.divide(10, 2));
+		System.out.println(calculator.divide(8, 4));
+		System.out.println(calculator.divide(20, 5));
+		System.out.println(calculator.subtract(12, 3));
+		System.out.println(calculator.subtract(6, 3));
+		System.out.println(calculator.subtract(300, 150));
 
 	}
 
