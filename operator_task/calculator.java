c55ee9ef0ab58969a32c41eb393a3df5af5fe522
daryl-bishop <daryl.bishop@hotmail.com>
2021-06-30 11:42:36 +0100

    added calculator

---
+++ operator_task/calculator.java
@@ -0,0 +1,9 @@
+package com.qa.operator_task;
+
+public class calculator {
+
+	public static int add(int a, int b) {
+		int sum = a + b;
+		return sum;
+	}
+}
---
+++ operator_task/operator_task.java
@@ -0,0 +1,12 @@
+package com.qa.operator_task;
+
+public class operator_task {
+
+	public static void main(String[] args) {
+		System.out.println(calculator.add(1, 2));
+		System.out.println(calculator.add(11, 2));
+		System.out.println(calculator.add(18, 2));
+
+	}
+
+}
